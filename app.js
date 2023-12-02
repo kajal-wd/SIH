873a7f8fa1b8c823d82b6482917fce606a077119
KAJAL SINGH <135737333+kajal-wd@users.noreply.github.com>
2023-12-02 13:24:00 +0530

    Add files via upload
    first upload files

---
+++ app.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true, 
+    navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"], // Customize navigation icons
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
